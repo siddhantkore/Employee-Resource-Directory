445d56e82b23deb7b08854f8af56878ed22e21e4
siddhant <siddhskore@gmail.com>
2026-09-12 18:55:23 +0530

    feat: dockerize the app and use postgres image & no local dependency

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const app = require("./app");
+const { testConnection } = require("./src/config/dbConfig");
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  await testConnection();
 });
